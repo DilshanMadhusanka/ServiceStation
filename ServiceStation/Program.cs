030d910b4f32f454e060e964a3c76766cec262e6
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-13 22:27:24 +0530

    configurations

--- ServiceStation/Program.cs
+++ ServiceStation/Program.cs
@@ -1,14 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);    // default builder. web application ekak hadal denn mekat daganna 
 
-
 builder.Services.AddControllersWithViews(); // MVC application ekaka base eka " AddControllersWithViews"
 
 var app = builder.Build();  // build the application 
 
 /* ude  line thunen wenne builder ekak hadanna. ekat ControllersWithViews tika add krann. ita passe build kranna.*/
 
+
+
+
 //-------------------------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+// me kalla development environment eka
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,15 +27,31 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
 
-app.UseAuthorization();
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+/* "use" kiyal thiyan ewa middlwear. hama request, respons ekema middlewear ekak run wenwa. user ha controller athara thiyenne */
+
+app.UseHttpsRedirection(); // mona url eka gahuwath https walat convert kranna 
+app.UseStaticFiles();  // www root folder eke thiayan css , js, lib, imges wage dewal use krannna
+
+app.UseRouting();  // path eka 
+
+app.UseAuthorization();  // katada access thiyenn and e monda 
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+app.Run();  // app eka run kranna 
